92b99f6bd506a8fd4d009986170fa4e783230198
Tawseef Ul Islam <70596049+Tawseef-Tonoy@users.noreply.github.com>
2023-12-22 12:42:51 +0600

    Update bandits.sh

--- bandits.sh
+++ bandits.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# Prompt for username
 read -p "Enter your Bandit project username: " username
-
-# Prompt for password
 read -s -p "Enter your Bandit project password: " password
 echo
-
-# Set the server address and port
 server="bandit.labs.overthewire.org"
 port=2220
-
-# SSH login command
 sshpass -p "$password" ssh -o StrictHostKeyChecking=no -p $port "$username@$server"
